a1b7426115e0e7831625e4e0339e2f08a8d49901
John Anderson <nhojanderson8@gmail.com>
2020-12-09 12:54:37 -0600

    Add files via upload
    Got The ADI method in place, I think I have an error in the first part of it though. The n to n+1/2 part

--- Anderson_John_Project.py
+++ Anderson_John_Project.py
@@ -31,33 +31,41 @@ b_x = 2*math.pi
 b_y = 2*math.pi
 
 #Boundary Conditions
-
 #TOP: u(x,y=by)=f_a(x)
 #BOTTOM: u(x,y=ay)=ga(x)
 #LEFT: NEUMANN: dudx(x=ax)=0
-
+#defining v here to be consistent and changeable
+v=0
 #RIGHT: This is a the big one 
 #g_a(bx) +(y-a_y)/(b_y-a_y) *[f_a(b_x)-g_a(b_x)]
-
-#that line reminds me of how alot of the non dimensional equations end up
-#like (T-To)/(T1-To)
-
-#given functions
-#g_a and f_a I will just call g and f
-#g=(x-a_x)^2*cos(x)
-#f=x*(x-a_x)^2  
 
 #Initial conditions, are just zero for all points, INSIDE the boundary
 #U(x,y,t)=0
 #I will still define this.
 Uo=0
 
-#I want to really make things as general as possible, so I could easily adapt this do a different problem.
-#that may mean making functions for things that dont  necessarily need functions
-#and reassigning zero as zero.
-#I want this easily adaptable
 
-#Functions
+#Defining Functions
+
+def thomas_alg_func(a,b,c,f):
+    """solves tridiagonal matrix"""
+#inputs are vectors containing the tridiagonal elements and right hand side
+    N=len(a)
+#storage vectors    
+    u_appx = [0]*N
+    alpha = [0]*N
+    g = [0]*N
+#Following the pseudocode
+#Zeroth element of this list corresponds to the first subscript in Thomas Algorithm
+    alpha[0] = a[0]
+    g[0] = f[0]
+    for j in range(1, N):
+        alpha[j] = a[j]-(b[j]/alpha[j-1])*c[j-1]
+        g[j] = f[j]-(b[j]/alpha[j-1])*g[j-1]
+    u_appx[N-1] = g[N-1]/alpha[N-1]
+    for j in range(1, N):
+        u_appx[-1-j] = (g[-1-j]-c[-1-j]*u_appx[-j])/alpha[-1-j]
+    return u_appx    
 
 #lets bring in that discretize the interval function
 def DIF(L ,N):
@@ -86,16 +94,9 @@ def given_g(x,a):
     return func_vals   
 
 #im gonna call this function RIGHT for now
-#it will just have to call the other functions
-# im not liking this tho, it will have to call those other functions with just a single 
-# y value
-# and i like my functions to be self contained.....
-#it doesnt have to call those other functions...... 
-#because its just a constant
-#I will just define those inside the function.
 
 def RIGHT(y, a_x, a_y, b_x, b_y):
-    """returns the RIGHT Boundary conditions values for this problem"""    
+    """returns the RIGHT Boundary condition values for this problem"""    
 #inputs are the boundary points of the domainand the discretized y values
 # I will just break this equation up for now     
     uno = cos(b_x)*(b_x-a_x)*(b_x-a_x)
@@ -104,14 +105,13 @@ def RIGHT(y, a_x, a_y, b_x, b_y):
     func_vals= [uno + dos*tres for dos in dos]
     return func_vals  
 
-#ok next
-
 #Number of discretized points
-
 #Number of internal x points
 N_x=8    
 #Number of internal y points
-N_y=8    
+N_y=8
+#just using the same number of points for x and y beccareful with this. i will have to go back and change later...maybe    
+N=N_x
     
 #calling the DIF
 x, dx = DIF(b_x,N_x)
@@ -121,56 +121,8 @@ y, dy = DIF (b_y,N_y)
 len_x=len(x)
 len_y=len(y)
 
-#setting up solution matrix....I really need a 3D matrix
-#Do i though...yes If I want to be able to plot the x and y for any time t
-#but if i just want the steady steady state I geuss I really only need 2 2D matrices
-#one for time n and one for time n+dt
-#hmmmmmm amd I dont know the the length of the 3rd dimension either.....
-
-#this kinda seems like an important point. I can solve it with 2 matrices.
-# but how will I be able to plot every u(x,y,t), I mean, I can make a matrix that keeps
-# on extending in the third dimension but that is inefeccient
-# I geuss I can just solve it, determine the length of third dimension. then solve it 
-#again and store it. but that seems like Im doing twice the calculations.
-
-#And im gonna need a while loop too.
-
-#hmm lets think about this......I wont really ever need to plot all the values,
-#only select values which I could just call again as needed....
-#No but If say i wanted to plot the middle of the time interval I would need to start all the 
-#way over to reach that value
-#the self appening matrix seems like a good idea, even if it is inefficient.
-#or is solving and then resolving better? I dont know
-#what is worse, running all the algorithms an additional time or 
-#continuolsy reallocating storage space for the 3rd dimension of the matrix
-#probalby the second option is worse.
-
-#okay that is how Im gonna proceed.
-#Sol_1 is my solution matrix for the nth time step and Sol 2 for the nth+1 time step
-#if Sol_1 appx Sol_@, steady state  is reached.
-#if not Sol_2 becomes Sol_1, and new Sol_2 is calulated
-
-#setting up solution matrices
-#Sol is my solution matrix for the nth time step and 
-#note im just using ones now so that when I apply the ICS I can see that it worked
+#Sol is solution matrix for the nth time step
 Sol = np.ones((len_x,len_y))
-##Sol next for the nth+1 time step
-#Sol_next = np.zeros((len_x,len_y))
-#why do I even use that shape function? idk it seems unnessary really.
-#Sol=np.zeros(shape=(len_t, len_x))
-
-#okay now I can actually start applying the numerical scheme
-
-#Applying Boundary Conditions
-#maybe I should make my solution matrices N_x by N_y so i dont have to continuosly
-#re apply the BCs.
-#And I see a better way to do this already, only make a new matrix when im applying the scheme.
-
-#And Im gonna have that same issue when I was making that maze script
-#the [0,0] element applies to the bottom left corner of the domain, 
-#but appears in the upper left corner of the matrix....
-#I would kinda like to work backwards but that I feel will be confusing to others
-#I can always just flip the matrix later.
 
 #Applying Boundary Conditions
 #Left, is the neumann
@@ -181,77 +133,133 @@ Sol[:,-1]=RIGHT(y, a_x, a_y, b_x, b_y)
 Sol[0,:]=given_g(x, a_x)
 #Top
 Sol[-1,:]=given_f(x, a_x)
-
-
 #Applying Initial condiots
-#Are already defined but I will just write it out and comment it
-# I will just redefine over this
 Sol[1:-1,1:-1]=Uo
-#now I have a row of ones that I will need to change suing the ghost node method
 
-##now I will define Sol_next
-##Sol next for the nth+1 time step
-##Sol_next = Sol Nope this wont work its the muteablilty thing in python
-##if i do this it treats them as the same object
-##thats one part of python I dont appreciate
-##Ok I found a different way copy()
-#
+#And i will do that right now...
+#approximating( u^n_0,k) with a three point forward approximation)
+#I will make a function for this and keep it here for now.
+def TPFF(col1,col2,v,dx):
+    N=len(col1)
+    col0=np.ones(N)
+    for k in range(0,N):
+        col0[k]=-3*col1[k]+4*col2[k]+v*2*dx
+    return col0
+
+##now I will define Sol_next, using copy()
 Sol_next=Sol.copy()
-
-#Okay on to the scheme! Do I wanna do CN in 2D with time, which would be a pentadiagonal
-#or the ADI, which would be essentially doing a tridiagonal twice.
-
-#I kinda wanna do pentadiagonal because I havent done that before.
-#im tired of the thomas algorithm
-
-#I could do both really.
-
-#the ADI is "elegant".
-
-#Im seeing this PTRANS-1 pop up, by some Saudis.
-#also a TW, two way approach "d designed for pairs of parallel computers"
-#Im gonna try this PTRANS-1 it looks pretty good
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# =============================================================================
-# different versions of given_x   
-# =============================================================================
-# =============================================================================
-# Dont worry abou this type of stuff for now. I do wanna try to implement these type of things later tho.   
-# =============================================================================
-##version 2 does this take advantage of parrelism?
-##    I dont think so so actually, because its not in a for loop, its in the "inline python" for loop
-##    uno=[x-a for x in x]
-##    dos=[uno*uno for x in uno]
-##    tres=[x*dos for x in x]     
-## version 3, doing the expansion my self, this i dont actually think is better
-##    func_vals=[x*x*x-2*a*x*2+a*a*x for x in x]
-##version 4 using a real for loop
-##    lenx=len(x)
-## for j in range (lenx)
-#    xi=x[j]
-#    uno=(xi-a)
-#    dos=uno*uno
-#    tres=xi*dos
-##and then I could also do loop unrolling
-##this seems like it is correct, it doesnt have to search the x vector multiple times per loop
-##it should take advantage of parrelism.
-#and remember the compiler does alot of this type of stuff automatically for me.     
-
-
-
+#im doing this before apply the 3 point forward method.
+Sol[1:-1,0]=TPFF(Sol[1:-1,1],Sol[1:-1,2],v,dx)      
     
-     
+ 
+
+# =============================================================================
+#  Neumann Boundary Condtion, Left, x=ax=0, GHOST NODE METHOD
+# =============================================================================
+
+#first im gonna define the Tridiagonal elements, which are the constants from the Crank Nicolson Scheme
+
+#also a delta t, and I geuss a D
+dt=0.1
+D=1
+#and dx=dy
+mu=D*dt/(dx*dx)
+#im using mu so I dont have to mispell lambda as lamda
+a=(1+mu)
+b=-mu/2
+c=-mu/2
+d=(1-mu)
+
+# =============================================================================
+# ADI-Method
+# =============================================================================
+#Half value matrix, for u(t=n+1/2)
+#Wait I should really change this HVM to be the same size as Sol, with the boundary conditions already applied
+HVM=Sol.copy()
+#its quicker just to make a copy rather tahn define a blank matrix and reapply the boundary conditions to the half value matrix
+#Step "A":The t=n to t=n+ 1/2 step.
+rhs_a=np.ones(N+1)
+#rhs for step a is (N+1) because of the ghost node
+
+
+#Pre thomas algorithm set upo
+#okay, the a,b,c and vectors, (N+1) because of ghost node
+#its fine that these are lists and not real numpy arrays
+#these dont change
+av= [a]*(N+1)
+bv= [b]*(N+1)
+#different first input because of ghost node
+bv[0]=b+c
+cv= [c]*(N+1)
+
+for m in range(0,N):
+
+#filling in the rhs vector   
+#Really i can just writre over the values as we go and define outside the loop  
+#I swiched to s and m rather than j adn k because i was getting confused for some reason before
+#Now I wanna switcfh back
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  keep for now, I dont like this any more
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#I wanna use j,k for sol and HVM, 
+#and then do things like rhs[j-1} becasue rhs is only internal points     
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#but for now I will just redefine    
+    rhs_a = np.ones(N+1)
+#first eq different    
+    rhs_a[0] = -b*Sol[m, 0] + d*Sol[m+1, 0] - c*Sol[m+2, 0]
+#middle eqs
+    for s in range(1, N):
+        rhs_a[s] = -b*Sol[m, s] + d*Sol[m+1, s] - c*Sol[m+2, s]
+#last eq different
+    rhs_a[-1] = -b*Sol[m,-2] + d*Sol[m+1,-2] - c*Sol[m+2,-2] - c*Sol[1+m,-1]     
+    HVM[m+1,0:-1]=thomas_alg_func(av,bv,cv,rhs_a)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#right now, this all the n+1/2 values,
+#the N+1 half point values can be gotten from the Sol matrix, they are right endpoints
+#same with bottom and top 
+
+#now, rhs_b is composed from these t= n+1/2 (half vals) 
+#I need to pay close attention to the order here. Which is why i stored them in that matrix.
+#I am filling up the t=n+1 matrix column by column
+    
+#Step "B" the t=n+1/2 to t=n+1
+#okay rhs b still is N long, I just need to do the steps N+1 one times
+#which means I need to redine my or make new thomas algorith vectors. I will just make new ones.
+av_b= [a]*(N)
+bv_b= [b]*(N)
+#different first input because of ghost node
+cv_b= [c]*(N)    
+    
+#I can either to it one column at a time or do them all at once.
+#    and I am solving for the u(x=0,k) points as well, so (N+1) by N for I make one long vector
+#rhs_b=np.ones((N+1)*N)    
+#lets just do one row right now
+rhs_b=np.ones(N)
+#first all the u(0,k) for k=1,2...N) really its Ny, I need to be careful here I was just using N above       
+#these are a little different because of the ghost node.
+#first equation different
+rhs_b[0]= -(b + c)*HVM[1, 1] + d*HVM[1, 0]  -b*Sol_next[0,0]
+#middle eqs
+for k in range(2, N):
+    rhs_b[k-1] = -(b + c)*HVM[k, 1] + d*HVM[k, 0]
+#last equation different
+    rhs_b[-1]= -(b + c)*HVM[N, 1] + d*HVM[N, 0] - c*Sol_next[N+1,0]
+Sol_next[1:-1,0]=thomas_alg_func(av_b,bv_b,cv_b,rhs_b)
+    
+#before I changed the indexes to m and s, because I didnt like seeing A[k,j] for example
+#but now I find it more confusing
+#Im gonna switch back to j and k, even though k will will come before j
+
+#now for the next ones, In a loop 
+rhs_b=np.ones((N+1))
+for j in range(1,N+1):      
+#first equation different
+    rhs_b[0]= -b*HVM[k, j-1] + d*HVM[k, j] - c*HVM[k, j+1] -b*Sol_next[k-1, j]
+#middle eqs
+    for k in range(2, N+1):
+        rhs_b[k-1] = -b*HVM[k, j-1] + d*HVM[k, j] - c*HVM[k, j+1]
+#last equation different
+    rhs_b[-1]= -b*HVM[k, j-1] + d*HVM[k, j] - c*HVM[k, j+1]-c*Sol_next[k-1, j]
+    Sol_next[1:-1,j]=thomas_alg_func(av_b,bv_b,cv_b,rhs_b)
