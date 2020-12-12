a046d6ae714e88967146d0424e812b61a7fa6c32
John Anderson <nhojanderson8@gmail.com>
2020-12-12 13:22:31 -0600

    Add files via upload
    The error must be in the ADI step A. It wasnt apparent before for smaller Lambda values. The penultimate row of the HVM, the u(x,y_N,t_n+1/2) intermediate values.

--- Anderson_John_Project.py
+++ Anderson_John_Project.py
@@ -106,9 +106,9 @@ def RIGHT(y, a_x, a_y, b_x, b_y):
 
 #Number of discretized points
 #Number of internal x points
-N_x=100
+N_x=254
 #Number of internal y points
-N_y=100
+N_y=254
 #just using the same number of points for x and y beccareful with this. i will have to go back and change later...maybe    
 N=N_x
     
@@ -150,7 +150,7 @@ def TPFF(col1,col2,v,dx):
 Sol_next=Sol.copy()
 #im doing this before apply the 3 point forward method.
 Sol[1:-1,0]=TPFF(Sol[1:-1,1],Sol[1:-1,2],v,dx)      
-    
+# still just is zero   
  
 
 # =============================================================================
@@ -158,9 +158,8 @@ Sol[1:-1,0]=TPFF(Sol[1:-1,1],Sol[1:-1,2],v,dx)
 # =============================================================================
 
 #first im gonna define the Tridiagonal elements, which are the constants from the Crank Nicolson Scheme
-
 #also a delta t, and I geuss a D
-dt=0.1
+dt=.1
 D=1
 #and dx=dy
 mu=D*dt/(dx*dx)
@@ -175,41 +174,35 @@ d=(1-mu)
 # =============================================================================
 #Half value matrix, for u(t=n+1/2)
 HVM=Sol.copy()
-
-#its quicker just to make a copy rather tahn define a blank matrix and reapply the boundary conditions to the half value matrix
-#
+#its quicker just to make a copy rather than define a blank matrix and reapply the boundary conditions to the half value matrix
 # =============================================================================
 # #Step "A":The t=n to t=n+ 1/2 step
 # =============================================================================
 rhs_a=np.ones(N+1)
 #rhs for step a is (N+1) because of the ghost node
-
-
 #Pre thomas algorithm set up
-#okay, the a,b,c and vectors, (N+1) because of ghost node
-#its fine that these are lists and not real numpy arrays
-#these dont change
 av= [a]*(N+1)
 bv= [b]*(N+1)
-#different first input because of ghost node
-#bv[0]=b+c
 cv= [c]*(N+1)
-#this actually needs to be cv[0], that shouldnt be the issue though
-cv[0]=b+c
-
+#different first input because of ghost node
+cv[0]=b+c 
+# =============================================================================
+# =============================================================================
+# The ERROR IS ACTUALLY IN HERE!
+# THE PENULTIMATE ROW IS JUST WRONG!!!!!!!! FIX IT
+# =============================================================================
+# =============================================================================
 #need to do this N times, for k=1,2..N
-#okay, this is why I was confused before, In the step B I have j as my outter loop index
-#and in step A I have k as my outter loop index.
-#I still need to call the correct elemetns though, it will just be different for both steps
-
-
 for k in range(1,N+1):
 ##for now I will just redefine inside for troubleshooting purposes    
     rhs_a = np.ones(N+1)
 ##first eq different    
-    rhs_a[0] = -b*Sol[k-1, 0] + d*Sol[k, 0] - c*Sol[k+1, 0]
+    rhs_a[0] = -b*Sol[k-1, 0] + d*Sol[k, 0] - c*Sol[k+1, 0] + b*v*2*dx
+#    b*v*2*dx from ghost node, even if it is zero
     ##middle eqs
     for j in range(1, N):
+##I think ive checked this already
+#    for j in range(1, N+1):
         rhs_a[j] = -b*Sol[k-1, j] + d*Sol[k, j] - c*Sol[k+1, j]
 ##last eq different
     rhs_a[-1] = -b*Sol[k-1,-2] + d*Sol[k,-2] - c*Sol[k+1,-2] - c*HVM[k,-1]   
@@ -219,54 +212,35 @@ for k in range(1,N+1):
 # =============================================================================
 # #Step "B" the t=n+1/2 to t=n+1
 # =============================================================================
-#!must solve one column at a time to preserve tridiagonal structure
-#rhs_b is composed from these t= n+1/2 (half vals) 
-rhs_b=np.ones(N)
-
+#must solve one column at a time to preserve tridiagonal structure
+#I would have to modify every Nth element of the b and c vectors, is fine for now
 #okay rhs b still is N long, I just need to do the steps N+1 one times    
-#which means I need to redine my or make new thomas algorith vectors. I will just make new ones.
+rhs_b=np.ones(N)
 av_b= [a]*(N)
 bv_b= [b]*(N)
 cv_b= [c]*(N)    
+# =============================================================================
+# I need to focus on this section here, for big mu values. THERE MUST BE AN ERROR
+# =============================================================================
 
-#first all the u(0,k) for k=1,2...N) really its Ny, I need to be careful here I was just using N above       
+#first all the u(0,k) for k=1,2...N)      
 #these are a little different because of the ghost node.
 #first equation different
-rhs_b[0]= -b*(HVM[1, 1]+2*dx*v) + d*HVM[1, 0] - c*HVM[1, 1] -b*Sol_next[0,0]
+rhs_b[0]= -b*(HVM[1, 1]-2*dx*v) + d*HVM[1, 0] - c*HVM[1, 1] -b*Sol_next[0,0]
 #middle eqs
 for k in range(2, N):
-    rhs_b[k-1] = -b*(HVM[k, 1]+2*dx*v) + d*HVM[k, 0] - c*HVM[k, 1]
+    rhs_b[k-1] = -b*(HVM[k, 1]-2*dx*v) + d*HVM[k, 0] - c*HVM[k, 1]
 #last equation different
-    rhs_b[-1]= -b*(HVM[N, 1]+2*dx*v) + d*HVM[N, 0] - c*HVM[N, 1] - c*Sol_next[N+1,0]
+    rhs_b[-1]= -b*(HVM[N, 1]-2*dx*v) + d*HVM[N, 0] - c*HVM[N, 1] - c*Sol_next[N+1,0]
 Sol_next[1:-1,0]=thomas_alg_func(av_b,bv_b,cv_b,rhs_b)
 
+
 #now for the next ones, u(j,k) for for j=1,2...N and k=1,2...N)
-
-
-#OKAY YES! This is now behaving as I was expecting. It was part a coding issue and part my own fault.
-#I had seperate parts for j=0 and j=1:N, and I think they both had a slight error in them. In one sectioon I wasnt
-#doing the k for 2,N and in the other I was doing 2,N+1 and rewriting over it.
-#The issue was I was using k as an idex in that first equation,  where k was still set to N from the previous loop ayayayayay
-#THAT WAS AN ISSUE
-#okay also wait now with out the sperate first part my u90,j) values dont look so hot. ah because im going j-1 which is accesing
-#the right boundary conditions,
-#So i either need to recode this or keep the two seperate, One for j=0, and one for j=1;N
-
-
 for j in range(1,N+1):
-# =============================================================================
-#     VERY IMPORTANT THIS NEXT LINE! AMATUER ISSUE BUT I FORGOT IT.
-#    ALWAYS PAY ATTENTION TO YOUR INDICES YOU JABRONI!
-# =============================================================================
-#!!!!!!!!!!
+#reset k to one at beginnig of this loop
     k=1
-#k needs to be reset to one before applying that first equation.    
-#for j in range(1,15):  
 #just redifining rhs_every time ever for now while im still trouble shooting   
-    rhs_b=np.ones((N))  
-#Okay I think that just fixed it, I still had N+1 right there! Lets hope so
-#No that wasnt it. For large N this is Wrong, the behavior near the Boundaries isnt what I would expect
-#maybe advancing the time much further will solve the problem but I dont think so    
+    rhs_b=np.ones((N))   
 #first equation different
     rhs_b[0]= -b*HVM[k, j-1] + d*HVM[k, j] - c*HVM[k, j+1] -b*Sol_next[k-1, j]
 #middle eqs
@@ -275,35 +249,212 @@ for j in range(1,N+1):
 #last equation different
     rhs_b[-1]= -b*HVM[N, j-1] + d*HVM[N, j] - c*HVM[N, j+1]-c*Sol_next[N+1, j]
     Sol_next[1:-1,j]=thomas_alg_func(av_b,bv_b,cv_b,rhs_b)
-#    
-#okay the issue is in the HVM, the values just arent correct. I will find what is wrong.
-#maybe not the middle values all approach zero    
-#but they are doing it way to quickly, the 2nd row and the penultimate row seem okay I guess
-#but the 3rd row and the 3rd to last row go to zero way too quickly for many differetn N's
-#it may be a coding error or maybe I appleid the scheme wrong :( :( ;(
-#it could be that three point forward method messing me up 
+  
+#Okay, why do I have this part B in two different steps? that could be a sign that something is wrong    
 
-#I dont think there was any issue. I got rid of the s and m indices and went back to j and k    
+#def ADI(Sol):
+#    """Quick and dirty ADI function right now so I can advance the solution forward a few time steps"""   
+#    # =============================================================================
+#    # ADI-Method
+#    # =============================================================================
+#    #Half value matrix, for u(t=n+1/2)
+#    HVM=Sol.copy()
+#    #its quicker just to make a copy rather tahn define a blank matrix and reapply the boundary conditions to the half value matrix
+#    # =============================================================================
+#    # #Step "A":The t=n to t=n+ 1/2 step
+#    # =============================================================================
+#    rhs_a=np.ones(N+1)
+#    #rhs for step a is (N+1) because of the ghost node
+#    #Pre thomas algorithm set up
+#    av= [a]*(N+1)
+#    bv= [b]*(N+1)
+#    cv= [c]*(N+1)
+#    #different first input because of ghost node
+#    cv[0]=b+c 
+#    
+#    #need to do this N times, for k=1,2..N
+#    for k in range(1,N+1):
+#    ##for now I will just redefine inside for troubleshooting purposes    
+#        rhs_a = np.ones(N+1)
+#    ##first eq different    
+#        rhs_a[0] = -b*Sol[k-1, 0] + d*Sol[k, 0] - c*Sol[k+1, 0] + b*v*2*dx
+#    #    b*v*2*dx from ghost node, even if it is zero
+#        ##middle eqs
+#        for j in range(1, N):
+#    ##I think ive checked this already
+#    #    for j in range(1, N+1):
+#            rhs_a[j] = -b*Sol[k-1, j] + d*Sol[k, j] - c*Sol[k+1, j]
+#    ##last eq different
+#        rhs_a[-1] = -b*Sol[k-1,-2] + d*Sol[k,-2] - c*Sol[k+1,-2] - c*HVM[k,-1]   
+#        HVM[k,0:-1]=thomas_alg_func(av,bv,cv,rhs_a)
+#    #right now, this all the n+1/2 values,
+#     
+#    # =============================================================================
+#    # #Step "B" the t=n+1/2 to t=n+1
+#    # =============================================================================
+#    #!must solve one column at a time to preserve tridiagonal structure
+#    #I would have to modify every Nth element of the b and c vectors, is fine for now
+#    #okay rhs b still is N long, I just need to do the steps N+1 one times    
+#    rhs_b=np.ones(N)
+#    av_b= [a]*(N)
+#    bv_b= [b]*(N)
+#    cv_b= [c]*(N)    
+#    # =============================================================================
+#    # I need to focus on this section here, for big mu values. THERE MUST BE AN ERROR
+#    # =============================================================================
+#    #DO I really have to have two different parts here because of the ghost node? Yes I think so
+#    #lets let v=5 and see what happens
+#    #i really think I am applying the GBNT correctly. changing v inverts the final paraobla I get. 
+#    #though there still may be a a sign error? because if I do a positive v, as in like say
+#    #heat into the system my parabola inverts. where it seems like that should make my parabola get taller
+#    #oh never mind its just the sign convention on q probably...no still seems wrong. idk move on 
+#    
+#    
+#    #first all the u(0,k) for k=1,2...N)      
+#    #these are a little different because of the ghost node.??
+#    #first equation different
+#    rhs_b[0]= -b*(HVM[1, 1]-2*dx*v) + d*HVM[1, 0] - c*HVM[1, 1] -b*Sol_next[0,0]
+#    #middle eqs
+#    for k in range(2, N):
+#        rhs_b[k-1] = -b*(HVM[k, 1]-2*dx*v) + d*HVM[k, 0] - c*HVM[k, 1]
+#    #last equation different
+#        rhs_b[-1]= -b*(HVM[N, 1]-2*dx*v) + d*HVM[N, 0] - c*HVM[N, 1] - c*Sol_next[N+1,0]
+#    Sol_next[1:-1,0]=thomas_alg_func(av_b,bv_b,cv_b,rhs_b)
+#    
+#    
+#    #now for the next ones, u(j,k) for for j=1,2...N and k=1,2...N)
+#    for j in range(1,N+1):
+#    #reset k to one at beginnig of this loop
+#        k=1
+#    #just redifining rhs_every time ever for now while im still trouble shooting   
+#        rhs_b=np.ones((N))   
+#    #first equation different
+#        rhs_b[0]= -b*HVM[k, j-1] + d*HVM[k, j] - c*HVM[k, j+1] -b*Sol_next[k-1, j]
+#    #middle eqs
+#        for k in range(2, N):
+#            rhs_b[k-1] = -b*HVM[k, j-1] + d*HVM[k, j] - c*HVM[k, j+1]
+#    #last equation different
+#        rhs_b[-1]= -b*HVM[N, j-1] + d*HVM[N, j] - c*HVM[N, j+1]-c*Sol_next[N+1, j]
+#        Sol_next[1:-1,j]=thomas_alg_func(av_b,bv_b,cv_b,rhs_b)
+#    return Sol_next
     
-#you know what maybe advancing the time will make it better.
-#but lets first double check all the ADI part A stuff.  
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+##first Step, just to compare and understand
+#FS=Sol_next.copy()
+#for t in range (501):
+#    Sol_next=ADI(Sol_next)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+t=1
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
+#plotting 
+#bottom
+#fig1, ax1 = plt.subplots()
+#plt.grid(1)    
+#plt.plot(y,FS[0,:],'-b') 
+#plt.plot(y,FS[1,:],':r')
+#plt.xlabel('x')
+#plt.ylabel('u(x, y)')
+#ax1.legend(['u(x, a_y)','u(x, y_1)'])
+#ax1.title.set_text('At first time step \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+#                   %(round(dx,4),round(dt,4),round(mu,4)))  
+##top
+#fig2, ax2 = plt.subplots()
+#plt.grid(1)    
+#plt.plot(y,FS[-1,:],'-b') 
+#plt.plot(y,FS[-2,:],':r')
+#plt.xlabel('x')
+#plt.ylabel('u(x, y)')
+#ax2.legend(['u(x, b_y)','u(x, y_N)'])
+#ax2.title.set_text('At first time step \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+#                   %(round(dx,4),round(dt,4),round(mu,4)))  
+##left
+#fig3, ax3 = plt.subplots()
+#plt.grid(1)
+#plt.plot(y,FS[:,0],'-b') 
+#plt.plot(y,FS[:,1],':r')
+#plt.xlabel('y')
+#plt.ylabel('u(x, y)')
+#ax3.legend(['u(a_x, y)','u(x_1, y)'])
+#ax3.title.set_text('At first time step \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+#                   %(round(dx,4),round(dt,4),round(mu,4)))  
+##right
+#fig4, ax4 = plt.subplots()
+#plt.grid(1)    
+#plt.plot(y,FS[:,-1],'-b') 
+#plt.plot(y,FS[:,-2],':r')     
+#plt.xlabel('y')
+#plt.ylabel('u(x, y')
+#ax3.legend(['u(b_x, y)','u(x_N, y)'])  
+#ax3.title.set_text('At first time step \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+#                   %(round(dx,4),round(dt,4),round(mu,4)))  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 #fig, ax = plt.subplots()
 #plt.grid(1)
 ###plt.plot(y,Sol[4,:],'-.c')
 ###plt.plot(y,HVM[4,:],':r')
 ###plt.plot(y,Sol_next[4,:],'-b')
-#bottom    
+##bottom
+#fig1, ax1 = plt.subplots()
+#plt.grid(1)    
 #plt.plot(y,Sol_next[0,:],'-b') 
 #plt.plot(y,Sol_next[1,:],':r')
-#top    
+##top
+#fig2, ax2 = plt.subplots()
+#plt.grid(1)    
 #plt.plot(y,Sol_next[-1,:],'-b') 
 #plt.plot(y,Sol_next[-2,:],':r')
-#left
+##left
+#fig3, ax3 = plt.subplots()
+#plt.grid(1)
 #plt.plot(y,Sol_next[:,0],'-b') 
 #plt.plot(y,Sol_next[:,1],':r')
-#right    
-plt.plot(y,Sol_next[:,-1],'-b') 
-plt.plot(y,Sol_next[:,-2],':r')           
+##right
+#fig4, ax4 = plt.subplots()
+#plt.grid(1)    
+#plt.plot(y,Sol_next[:,-1],'-b') 
+#plt.plot(y,Sol_next[:,-2],':r')           
 ###    right now I think the issue is in the last equation, the u(N,k) equation
 ##ax.legend(['u(0,y,t^n)','u(x_1,y,t^n+1)'])
 #plt.xlabel('x')
@@ -314,3 +465,64 @@ plt.plot(y,Sol_next[:,-2],':r')
 #                   %(round(dx,4),round(dt,4),round(mu,4)))
 #ax.title.set_text('u(0,y,t^n) and u(x_1,y,t^n+1) \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
 #                   %(round(dx,4),round(dt,4),round(mu,4)))    
+    
+    
+ 
+#3D GRAPH
+from mpl_toolkits import mplot3d
+fig10 = plt.figure()
+ax10 = plt.axes(projection='3d')
+BIGX, BIGY = np.meshgrid(x, y)    
+from matplotlib import cm    
+surf = ax10.plot_surface(BIGX, BIGY, Sol_next, cmap=cm.viridis,
+                       linewidth=0, antialiased=False)
+#fig10.colorbar(surf, shrink=0.75, aspect=5)
+ax10.set_title('u(x,y,t=%s s) \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s '%(round(dt*t,4),round(dx,4),round(dt,4),round(mu,4)))
+ax10.set_xlabel('x')
+ax10.set_ylabel('y')
+ax10.set_zlabel('u')    
+#
+#
+#
+##more plotting
+t=1
+##bottom
+fig5, ax5 = plt.subplots()
+plt.grid(1)    
+plt.plot(y,Sol_next[0,:],'-b') 
+plt.plot(y,Sol_next[1,:],':r')
+plt.xlabel('x')
+plt.ylabel('u(x, y)')
+ax5.legend(['u(x, a_y)','u(x, y_1)'])
+ax5.title.set_text('BOTTOM At t= %s s \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+                   %(round(dt*t,4),round(dx,4),round(dt,4),round(mu,4)))  
+#top
+fig6, ax6 = plt.subplots()
+plt.grid(1)    
+plt.plot(y,Sol_next[-1,:],'-b') 
+plt.plot(y,Sol_next[-2,:],':r')
+plt.xlabel('x')
+plt.ylabel('u(x, y)')
+ax6.legend(['u(x, b_y)','u(x, y_N)'])
+ax6.title.set_text('TOP: At t= %s s \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+                   %(round(dt*t,4), round(dx,4),round(dt,4),round(mu,4)))  
+##left
+#fig7, ax7 = plt.subplots()
+#plt.grid(1)
+#plt.plot(y,Sol_next[:,0],'-b') 
+#plt.plot(y,Sol_next[:,1],':r')
+#plt.xlabel('y')
+#plt.ylabel('u(x, y)')
+#ax7.legend(['u(a_x, y)','u(x_1, y)'])
+#ax7.title.set_text('LEFT: At t= %s s \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+#                   %(round(dt*t,4), round(dx,4),round(dt,4),round(mu,4)))  
+#right
+fig8, ax8 = plt.subplots()
+plt.grid(1)    
+plt.plot(y,Sol_next[:,-1],'-b') 
+plt.plot(y,Sol_next[:,-2],':r')     
+plt.xlabel('y')
+plt.ylabel('u(x, y)')
+ax8.legend(['u(b_x, y)','u(x_N, y)'])  
+ax8.title.set_text('RIGHT: At t= %s s \n $\Delta$x=$\Delta$y=%s units, $\Delta$ t= %s s, $\mu$= %s'
+                   %(round(dt*t,4), round(dx,4),round(dt,4),round(mu,4)))
